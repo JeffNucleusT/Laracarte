808be5d46f9ff713fa1435322e000fbf9d667c3b
Jeffzer Creative <jeffserwork7@gmail.com>
2019-07-27 11:16:56 +0100

    Queue Mail for more performance

--- app/Mail/ContactMessageCreated.php
+++ app/Mail/ContactMessageCreated.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Models\Message;
 
-class ContactMessageCreated extends Mailable
+class ContactMessageCreated extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -31,6 +31,7 @@ class ContactMessageCreated extends Mailable
      */
     public function build()
     {
-        return $this->markdown('email.message.created');
+        return $this->from($this->message->email, $this->message->name)
+            ->markdown('email.message.created');
     }
 }
